477f6fba1d1586769a50acdc166a22cbc5c69da3
Daniel Rivero <danielcfgm@gmail.com>
2020-02-21 23:01:43 +0100

    First commit

---
+++ src/main/java/com/trifulca/OnePieceDB/OptcdbApplication.java
@@ -0,0 +1,13 @@
+package com.trifulca.OnePieceDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptcdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptcdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trifulca/OnePieceDB/OptcdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trifulca.OnePieceDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptcdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
